990cfee62bb59c1b2803562733083de7a5902873
juan <juandgoc@gmail.com>
2020-04-14 16:32:08 -0500

    added missing files

--- remindmoi_django/remindmoi_bot/models.py
+++ remindmoi_django/remindmoi_bot/models.py
@@ -5,7 +5,7 @@ class Reminder(models.Model):
     reminder_id = models.AutoField(primary_key=True)
 
     zulip_user_email = models.CharField(max_length=128)
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=1024)
     created = models.DateTimeField()
     deadline = models.DateTimeField()
     active = models.BooleanField(default=True)
